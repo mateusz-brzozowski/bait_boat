1f992caa163ad6a067079e6c6b12f7d9c2d7ba3b
Mateusz Brzozowski <01158596@pw.edu.pl>
2024-12-14 18:57:52 +0100

    add ngx-joystick

---
+++ node_modules/ngx-joystick/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+/// <amd-module name="ngx-joystick" />
+export * from './public-api';
---
+++ node_modules/ngx-joystick/lib/ngx-joystick.module.d.ts
@@ -0,0 +1,7 @@
+import * as i0 from "@angular/core";
+import * as i1 from "./ngx-joystick.component";
+export declare class NgxJoystickModule {
+    static ɵfac: i0.ɵɵFactoryDeclaration<NgxJoystickModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<NgxJoystickModule, [typeof i1.NgxJoystickComponent], never, [typeof i1.NgxJoystickComponent]>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<NgxJoystickModule>;
+}
