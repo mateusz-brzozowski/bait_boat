00b2ac22d871d290509edeb9850a1596ff12a229
Mateusz Brzozowski <01158596@pw.edu.pl>
2024-12-14 20:14:41 +0100

    add client connection to boat

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,26 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from '../environments/environment';
+import { Hotspot } from '@ionic-native/hotspot/ngx';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+const config: SocketIoConfig = { url: environment.socketUrl, options: {} };
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
+  ],
+  providers: [
+    Hotspot,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideHttpClient(withInterceptorsFromDi())
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- client/src/app/connect/connect.page.ts
+++ client/src/app/connect/connect.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Hotspot } from '@ionic-native/hotspot/ngx';
+import { ToastController } from '@ionic/angular';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-connect',
@@ -6,7 +10,60 @@ import { Component } from '@angular/core';
   styleUrls: ['connect.page.scss']
 })
 export class ConnectPage {
+  isConnecting: boolean = false;
+  isTesting: boolean = false;
 
-  constructor() {}
+  constructor(
+    private hotspot: Hotspot,
+    private toastController: ToastController,
+    private httpClient: HttpClient
+  ) { }
 
+  async connectToHotspot() {
+    this.isConnecting = true;
+    try {
+      await this.hotspot.connectToWifi(environment.serverSSID, environment.serverPassword);
+      this.showToast('Connected to Raspberry Pi Hotspot', 'success');
+    } catch (error) {
+      console.error('Error connecting to hotspot:', error);
+      this.showToast('Error connecting to hotspot: ' + error, 'danger');
+    } finally {
+      this.isConnecting = false;
+    }
+  }
+
+  async testConnection() {
+    this.isTesting = true;
+    this.httpClient.get(environment.socketUrl, { responseType: 'text' }).subscribe({
+      next: response => {
+        console.log('Response:', response);
+        this.showToast('Connection successful: ' + response, 'success');
+        this.isTesting = false;
+      },
+      error: error => {
+        console.error('Error:', error);
+        this.showToast('Connection failed: ' + error.message, 'danger');
+        this.isTesting = false;
+      },
+      complete: () => {
+        console.log('Request complete');
+      }
+    });
+  }
+
+  private async showToast(message: string, color: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      color: color,
+      position: 'top',
+      buttons: [
+        {
+          text: 'Dismiss',
+          role: 'cancel'
+        }
+      ]
+    });
+    toast.present();
+  }
 }
