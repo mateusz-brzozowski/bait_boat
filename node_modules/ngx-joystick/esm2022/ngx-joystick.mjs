81e6ca94c3293271c70d8f2aa715b24009700ce3
Mateusz Brzozowski <01158596@pw.edu.pl>
2024-12-15 17:48:51 +0100

    remove filed due to gitignore

--- node_modules/ngx-joystick/esm2022/ngx-joystick.mjs
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmd4LWpveXN0aWNrLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vcHJvamVjdHMvbmd4LWpveXN0aWNrL3NyYy9uZ3gtam95c3RpY2sudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0dBRUc7QUFFSCxjQUFjLGNBQWMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogR2VuZXJhdGVkIGJ1bmRsZSBpbmRleC4gRG8gbm90IGVkaXQuXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9wdWJsaWMtYXBpJztcbiJdfQ==
--- node_modules/ngx-joystick/public-api.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/ngx-joystick.component';
-export * from './lib/ngx-joystick.module';
