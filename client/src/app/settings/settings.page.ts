3944399b86bc84cc629a18dba59e86c207d4cd43
Mateusz Brzozowski <01158596@pw.edu.pl>
2024-12-19 17:51:13 +0100

    base settings

--- client/src/app/settings/settings.page.ts
+++ client/src/app/settings/settings.page.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { WebsocketService } from '../services/websocket.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: 'settings.page.html',
   styleUrls: ['settings.page.scss']
 })
-export class SettingsPage {
+export class SettingsPage implements OnInit {
+  motorsSpeed: number = 0;
 
-  constructor() {}
+  ngOnInit() {
+    this.websocketService.listenForEvent('message').subscribe((response: any) => {
+      if (response.error) {
+        console.log('Message from server:', response);
+        this.showToast(response.error, 'warning');
+      }
+    });
+  }
 
+  constructor(private toastController: ToastController,
+    private websocketService: WebsocketService) { }
+  
+  pinFormatter(value: number) {
+    return `${value*10}%`;
+  }
+
+  onSpeedChange() {
+    this.websocketService.sendEvent('set_motors_speed', { speed: this.motorsSpeed });
+  }
+
+  private async showToast(message: string, color: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      color: color,
+      position: 'top',
+      positionAnchor: 'header',
+      buttons: [
+        {
+          text: 'Dismiss',
+          role: 'cancel'
+        }
+      ]
+    });
+    toast.present();
+  }
 }
